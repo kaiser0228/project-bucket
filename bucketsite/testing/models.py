78fd1ee7e7876efa890d6039dc4bff9fff610791
Dumplin <avghdev@gmail.com>
2018-03-24 15:28:11 -0700

    Adding files

--- bucketsite/testing/models.py
+++ bucketsite/testing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ListItem(models.Model):
+    title = models.CharField(max_length = 200)
+    author = models.CharField(max_length = 100)
+    votes = models.PositiveIntegerField(default = 0)
+    pub_date = models.DateTimeField("published date", default = timezone.now())
+
+    def __str__(self):
+        return self.title
